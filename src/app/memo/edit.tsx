1617d03de1e8e27c49e7e43663d7c7b58fb4740b
kumabiko <nousbear.dev@gmail.com>
2024-06-16 10:04:42 +0900

    ✨ add memo CRUD

---
+++ src/app/memo/edit.tsx
@@ -0,0 +1,88 @@
+import { View, TextInput, StyleSheet, Alert } from "react-native";
+import { router, useLocalSearchParams } from "expo-router";
+import { useState, useEffect } from "react";
+import { doc, getDoc, setDoc, Timestamp } from "firebase/firestore";
+
+import KeyboardAvoidingView from "@/components/KeyboardAvoidingView";
+import CircleButton from "@/components/CircleButton";
+import Icon from "@/components/Icon";
+import { auth, db } from "@/config";
+
+const handlePress = (id: string, bodyText: string): void => {
+  if (auth.currentUser === null) {
+    return;
+  }
+  const ref = doc(db, `users/${auth.currentUser.uid}/memos`, id);
+  setDoc(ref, {
+    bodyText,
+    updatedAt: Timestamp.fromDate(new Date()),
+  })
+    .then(() => {
+      router.back();
+    })
+    .catch((error) => {
+      console.log(error);
+      Alert.alert("更新に失敗しました");
+    });
+};
+
+const Edit = (): JSX.Element => {
+  const id = String(useLocalSearchParams().id);
+  const [bodyText, setBodyText] = useState("");
+  useEffect(() => {
+    if (auth.currentUser === null) {
+      return;
+    }
+    const ref = doc(db, `users/${auth.currentUser.uid}/memos`, id);
+    getDoc(ref)
+      .then((docRef) => {
+        const RemoteBodyText = docRef?.data()?.bodyText;
+        setBodyText(RemoteBodyText);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
+
+  return (
+    <KeyboardAvoidingView style={styles.container}>
+      <View style={styles.inputContainer}>
+        <TextInput
+          multiline
+          style={styles.input}
+          value={bodyText}
+          onChangeText={(text) => {
+            setBodyText(text);
+          }}
+          autoFocus
+        />
+      </View>
+      <CircleButton
+        onPress={() => {
+          handlePress(id, bodyText);
+        }}
+      >
+        <Icon name="check" size={40} color="#ffffff" />
+      </CircleButton>
+    </KeyboardAvoidingView>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  inputContainer: {
+    flex: 1,
+  },
+  input: {
+    flex: 1,
+    textAlignVertical: "top",
+    fontSize: 16,
+    lineHeight: 24,
+    paddingVertical: 32,
+    paddingHorizontal: 27,
+  },
+});
+
+export default Edit;
---
+++ src/app/memo/list.tsx
@@ -0,0 +1,67 @@
+import { View, StyleSheet, FlatList } from "react-native";
+import { router, useNavigation } from "expo-router";
+import { useEffect, useState } from "react";
+import { collection, onSnapshot, query, orderBy } from "firebase/firestore";
+
+import CircleButton from "@/components/CircleButton";
+import Icon from "@/components/Icon";
+import LogOutButton from "@/components/LogOutButton";
+import { db, auth } from "@/config";
+import { type Memo } from "@/types/memo";
+import MemoListItem from "@/components/MemoLitsItem";
+
+const handlePress = (): void => {
+  router.push("/memo/create");
+};
+
+const List = (): JSX.Element => {
+  const [memos, setMemos] = useState<Memo[]>([]);
+  const navigation = useNavigation();
+  useEffect(() => {
+    navigation.setOptions({
+      headerRight: () => {
+        return <LogOutButton />;
+      },
+    });
+  }, []);
+  useEffect(() => {
+    if (auth.currentUser === null) {
+      return;
+    }
+    const ref = collection(db, `users/${auth.currentUser.uid}/memos`);
+    const q = query(ref, orderBy("updatedAt", "desc"));
+    const unsubscribe = onSnapshot(q, (snapshot) => {
+      const remoteMemos: Memo[] = [];
+      snapshot.forEach((doc) => {
+        const { bodyText, updatedAt } = doc.data();
+        remoteMemos.push({
+          id: doc.id,
+          bodyText,
+          updatedAt,
+        });
+      });
+      setMemos(remoteMemos);
+    });
+    return unsubscribe;
+  }, []);
+  return (
+    <View style={styles.container}>
+      <FlatList
+        data={memos}
+        renderItem={({ item }) => <MemoListItem memo={item} />}
+      />
+      <CircleButton onPress={handlePress}>
+        <Icon name="plus" size={40} color="#ffffff" />
+      </CircleButton>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#ffffff",
+  },
+});
+
+export default List;
